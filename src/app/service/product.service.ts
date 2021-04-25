92399ae105ae922abea06c523f5172a954d33ffb
Győrffy Péter <66072584+Exterior7@users.noreply.github.com>
2021-04-25 21:02:10 +0200

    Service

--- src/app/service/product.service.ts
+++ src/app/service/product.service.ts
@@ -1,9 +1,45 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { Product } from '../model/product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  apiUrl: string = "https://nettuts.hu/jms/Exterior7/products";
+
+  list$: BehaviorSubject<Product[]> = new BehaviorSubject<Product[]>([]);
+
+  constructor(
+    private http: HttpClient,
+  ) { }
+
+    // CRUD: Update
+
+    getAll():void {
+      this.http.get<Product[]>(this.apiUrl).subscribe(
+        products => this.list$.next(products)
+      )
+    }
+
+    get(product: Product): Observable<Product> {
+      return this.http.get<Product>(`${this.apiUrl}/${product.id}`)
+  }
+
+    remove(product: Product): void {
+      this.http.delete<Product>(`${this.apiUrl}/${product.id}`).subscribe(
+        () => this.getAll()
+      )
+    }
+
+    create(product: Product): Observable<Product> {
+      return this.http.post<Product>(this.apiUrl, product);
+    }
+
+    update(product: Product): Observable<Product> {
+      return this.http.patch<Product>(`${this.apiUrl}/${product.id}`, product);
+    }
+
 }
